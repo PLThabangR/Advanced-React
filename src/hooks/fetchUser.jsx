e31fcb9b020fe2711a4fa7c1fb0dbfb182a71dc5
PLThabangR <rakgoropthabang@gmail.com>
2024-06-07 11:00:47 +0200

    Using useEffect  to  fetch  user data from the API

---
+++ src/hooks/fetchUser.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function FetchUser() {
+  const [user, setUser] = React.useState([]);
+
+  const fetchData = () => {
+
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  return Object.keys(user).length > 0 ? (
+    <div>
+      <h1>Customer data</h1>
+
+    </div>
+  ) : (
+    <h1>Data pending...</h1>
+  );
+}
+
+export default FetchUser;
